ea2dad6738a7916375cacf493282a6d0b5501b2f
summ791 <rithanyagopinathan2005@gmail.com>
2025-06-11 13:06:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { useState } from "react";
+
 import Welcome from "@/pages/welcome";
 import Layout from "@/components/layout";
 import Home from "@/pages/home";
 import Booking from "@/pages/booking";
 import Profile from "@/pages/profile";
 import NotFound from "@/pages/not-found";
+import Header from "./components/Header";
 
 function Router() {
   const [showWelcome, setShowWelcome] = useState(true);
@@ -19,14 +21,17 @@ function Router() {
   }
 
   return (
-    <Layout>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/booking" component={Booking} />
-        <Route path="/profile" component={Profile} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
+    <>
+      <Header />
+      <Layout>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/booking" component={Booking} />
+          <Route path="/profile" component={Profile} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </>
   );
 }
 
