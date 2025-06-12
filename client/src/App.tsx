55560668989abcac48824552c3da1f5cab7954a6
summ791 <rithanyagopinathan2005@gmail.com>
2025-06-12 20:21:39 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { useState } from "react";
 
 import Welcome from "@/pages/welcome";
@@ -38,7 +37,6 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
         <Toaster />
         <Router />
       </TooltipProvider>
