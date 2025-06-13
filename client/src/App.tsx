cc3cc6a4595fac916c3b1dfb0a998fcd735e5635
summ791 <rithanyagopinathan2005@gmail.com>
2025-06-13 13:37:57 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { TooltipProvider } from "@/components/ui/tooltip"; 
 import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { useState } from "react";
 
 import Welcome from "@/pages/welcome";
