f7ad4eb785e69200fdd3ecece207ef9a7f7c282e
summ791 <rithanyagopinathan2005@gmail.com>
2025-06-13 13:42:21 +0530

    Create tooltip.tsx

---
+++ client/src/components/ui/tooltip.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger };
